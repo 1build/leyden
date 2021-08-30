52f4efb7a7ded9f64858921ff75915ecf9a3b0fd
Michael Darr <michael.e.darr@gmail.com>
2021-08-30 15:56:27 -0400

    Set up build process
    Signed-off-by: Michael Darr <michael.e.darr@gmail.com>

--- packages/datum/src/index.ts
+++ packages/datum/src/index.ts
@@ -1,1 +1,13 @@
-export const testExport = 'tester';
+export interface Hello {
+    something: string;
+}
+
+export const testExport: Hello = {
+    something: 'tester',
+};
+
+export const blah = (something: string): Hello => {
+    return  {
+        something,
+    };
+};
