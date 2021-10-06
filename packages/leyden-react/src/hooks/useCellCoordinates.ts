8349f14d1c328266bbb08c877037e771f42b03c6
Michael Darr <michael.e.darr@gmail.com>
2021-10-06 19:31:52 -0400

    Throttle coordinate recalculation
    Signed-off-by: Michael Darr <michael.e.darr@gmail.com>

--- packages/leyden-react/src/hooks/useCellCoordinates.ts
+++ packages/leyden-react/src/hooks/useCellCoordinates.ts
@@ -11,6 +11,7 @@ import { ReactEditor } from '../plugin/ReactEditor';
 
 export const useCellCoordinates = (cell: Cell<CellType>): Coordinates|null => {
     const editor = useLeydenStatic();
+    const [refreshTimeoutID, setRefreshTimeoutID] = useState<ReturnType<typeof setTimeout>|null>(null);
 
     const [coordinates, setCoordinates] = useState(ReactEditor.cellCoords(editor, cell));
 
@@ -19,17 +20,23 @@ export const useCellCoordinates = (cell: Cell<CellType>): Coordinates|null => {
             return;
         }
         const unsubscribe = LeydenEditor.subscribeToOperations(editor, op => {
+            if (refreshTimeoutID !== null) {
+                return;
+            }
             if (!LeydenEditor.operationMovesCoords(editor, op, coordinates)) {
                 return;
             }
             // If not wrapped in `setTimeout`, this runs before the cell paths are updated
-            // and coordinate movement is never detected. 
-            setTimeout(() => {
+            // and coordinate movement is never detected.
+            // To prevent rapid re-calculation of coordinates when events like row deletion occur, a
+            // throttler limits refresh frequency to 4 per second.
+            setRefreshTimeoutID(setTimeout(() => {
+                setRefreshTimeoutID(null);
                 const newCoords = ReactEditor.cellCoords(editor, cell);
                 if (newCoords === null || !Coordinates.equals(coordinates, newCoords)) {
                     setCoordinates(newCoords);
                 }
-            });
+            }, 250));
         });
         return () => {
             unsubscribe();
