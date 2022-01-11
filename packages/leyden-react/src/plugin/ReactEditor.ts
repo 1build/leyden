b86ad64a2b53ffbb881debfe6750ee8dbdbfaf08
Michael Darr <michael.e.darr@gmail.com>
2022-01-11 12:52:41 -0500

    Improve safety of cell coordinate access
    Signed-off-by: Michael Darr <michael.e.darr@gmail.com>

--- packages/leyden-react/src/plugin/ReactEditor.ts
+++ packages/leyden-react/src/plugin/ReactEditor.ts
@@ -11,13 +11,18 @@ export interface ReactEditor extends LeydenEditor, Omit<SlateReactEditor, 'child
 export const ReactEditor = {
     /**
      * Get the coordinates of a cell.
+     * If `node` is not found within `editor`, `null` is returned. 
      */
 
     cellCoords: (
         editor: Editor,
         node: Descendant,
     ): Coordinates|null => {
-        const cellPath = SlateReactEditor.findPath(editor, node);
-        return LeydenEditor.pathCoords(editor, cellPath);
+        try {
+            const cellPath = SlateReactEditor.findPath(editor, node);
+            return LeydenEditor.pathCoords(editor, cellPath);
+        } catch {
+            return null;
+        }
     },
 };
